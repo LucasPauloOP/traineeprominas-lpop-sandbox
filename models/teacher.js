c239f0cb900a023e83d3dc53338500eb3c06a25a
lucaspauloop <lucaspaulodeoliveira@outlook.com>
2019-06-04 17:44:25 -0300

    gets teacher

---
+++ models/teacher.js
@@ -0,0 +1,58 @@
+const mongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+const mdbURL='mongodb+srv://lucaspauloop:Lucio3237*@cluster0-5y6gh.mongodb.net/test?retryWrites=true';
+var id;
+let db;
+let teacherCollection;
+
+mongoClient.connect(mdbURL, { native_parser: true }, (err, database) => {
+
+    if (err) {
+        console.error('Ocorreu um erro ao conectar ao mongoDB', err);
+        // send.status(500);
+    }
+    else {
+        console.log('User CONECTOU!');
+
+        db = database.db("trainee-prominas");
+        teacherCollection = db.collection('teacher');
+
+        teacherCollection.countDocuments().then((count) => {
+            id = count;
+            console.log(count);
+        });
+    }
+});
+
+
+//----------getalll----------------------
+exports.getall=(status,project)=>{
+    console.log('------>model',status);
+    //console.log(userCollection.count())
+    return teacherCollection.find(status,project).toArray();
+
+};
+
+//-----------getone---------------------
+exports.getone=(where,project)=>{
+    return teacherCollection.find(where, project).toArray();
+};
+/*
+//------------post----------------------
+exports.post=(newUser)=>{
+    newUser.id=++id;
+    console.log('------>',newUser);
+    return userCollection.insertOne(newUser);
+};
+
+//-------------put----------------------
+exports.put=(newUser,where,project)=>{
+    console.log('--------->',newUser);
+    return userCollection.findOneAndUpdate(where,{$set:{...newUser}});
+};
+
+//---------------delete-------------------
+exports.delete=(where)=>{
+    return userCollection.findOneAndUpdate(where,{ $set: { status: 0 } });
+};*/
